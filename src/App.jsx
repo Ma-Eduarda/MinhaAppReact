aaa5c6b70a926ab5faae41549ad3cd267f930e18
Ma-Eduarda <eduardabritto2014@gmail.com>
2025-05-25 22:38:37 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,15 @@ import Header from './Components/Header.jsx'
 import Footer from './Components/Footer.jsx'
 import Main from './Components/Main.jsx'
 
-
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
     <div className="page-container">
-        <Header/>
-        <Main/>
-        <div className="content-wrap"></div>
-        <Footer/>
-      </div>
-      <div/>
-    </>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
   )
 }
 
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,12 @@
 import Carousel from "react-bootstrap/Carousel";
-import img1 from '../images/img1.jpg';
-import img2 from '../images/img2.jpg';
-import img3 from '../images/img3.jpg';
-
+import img1 from "../images/img1.jpg";
+import img2 from "../images/img2.jpg";
+import img3 from "../images/img3.jpg";
 
 const Main = () => {
     return (
         <main>
-            <div style={{maxWidth: "800px", margin: "2em auto", marginTop: "50px" }}>
+            <div style={{maxWidth: "800px", margin: "2em auto", marginTop: "50px", marginBottom: "50px"}}>
                 <Carousel>
                     <Carousel.Item>
                         <img
@@ -41,13 +40,45 @@ const Main = () => {
                         />
                         <Carousel.Caption>
                             <h3>Third slide label</h3>
-                            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                            <p>
+                                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                            </p>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
+
+            <section
+                style={{marginTop: "50px", textAlign: "center", maxWidth: "800px", marginLeft: "auto", marginRight: "auto", padding: "0 1em", fontSize: "1em"
+                }}
+            >
+                <h2 style={{ fontSize: "2rem", marginBottom: "1em" }}>About us</h2>
+                <p style={{ lineHeight: "1.6" }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia vel
+                    reiciendis dolorem vero soluta accusamus, natus voluptatem impedit.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+                    voluptas ipsa saepe sunt! Ullam nesciunt, ad voluptas sequi quae
+                    voluptate veritatis officiis laboriosam! Maxime ex fuga ad nemo
+                    commodi sapiente! Lorem, ipsum dolor sit amet consectetur adipisicing
+                    elit.
+                </p>
+
+                    <br />
+
+                <p style={{ lineHeight: "1.6" }}>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis,
+                    voluptatibus fugit quas, voluptatum mollitia repudiandae omnis, maxime
+                    aliquid iure illo officiis? Est quidem modi nostrum necessitatibus
+                    corporis, voluptates nesciunt. Totam? Lorem, ipsum dolor sit amet
+                    consectetur adipisicing elit. Quae, neque labore! Sed, earum. Ducimus,
+                    facere consequatur repellendus expedita libero ab cumque beatae
+                    tempore laborum dicta. Reprehenderit exercitationem minima accusamus
+                    velit!
+                </p>
+            </section>
+            <br />
         </main>
-    )
+    );
 };
 
 export default Main;
